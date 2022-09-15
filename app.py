aa3e3036fcd21f97727b5b6b145b6fc962158366
abhis1223 <57350784+abhis1223@users.noreply.github.com>
2022-09-15 09:55:10 +0530

    Frontend

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,redirect
 
 app = Flask(__name__)
+app.config.update(
+    TESTING=True,
+    SECRET_KEY='opds'
+)
 
-@app.route("/")
-def hello_world():
-    return "<h2> Running Workflow UserChange</h2>"
+@app.route('/', methods=['GET','POST'])
+def login():
+    user={"name":"Abhi"}
+    return render_template("index.html", user=user)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
